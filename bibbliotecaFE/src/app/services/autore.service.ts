45ae69e1057b9705ade07f052c2bac995eff1720
GianmarcoGrimaldi <gianmarcogrimaldi.gg@gmail.com>
2023-07-27 11:39:18 +0200

    commit

--- bibbliotecaFE/src/app/services/autore.service.ts
+++ bibbliotecaFE/src/app/services/autore.service.ts
@@ -1,14 +1,15 @@
+import { Injectable } from'@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Autore } from '../models/autore';
+import { Observable } from 'rxjs';
 
 const urlLocalHost = 'http://localhost:8080';
 const requestMapping = '/api/autore';
 const getMapping = '/get/all';
+const postMapping = '/save';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AutoreService {
 
@@ -17,4 +18,11 @@ export class AutoreService {
   getAllAutori(): Observable<Autore[]> {
     return this.http.get<Autore[]>(urlLocalHost + requestMapping + getMapping);
   }
+
+  saveAutore(autore: Autore): Observable<Autore> {
+    console.log("Siamo nel service saveAutore");
+    console.log(autore);
+    return this.http.post<any>(urlLocalHost + requestMapping + postMapping, autore)
+  }
+
 }
--- bibbliotecaFE/src/app/services/editore.service.ts
+++ bibbliotecaFE/src/app/services/editore.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Editore } from '../models/editore';
+import { Observable } from 'rxjs';
+
+const urlLocalHost = 'http://localhost:8080';
+const requestMapping = '/api/editore';
+const getMapping = '/get/all';
+const postMapping = '/save';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EditoreService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getAllEditore(): Observable<Editore[]> {
+    return this.http.get<Editore[]>(urlLocalHost + requestMapping + getMapping);
+  }
+
+  saveAutore(editore: Editore ): Observable<Editore> {
+    console.log("Siamo nel service saveLibro");
+    console.log(editore);
+    return this.http.post<any>(urlLocalHost + requestMapping + postMapping, editore)
+  }
+
+
+
+
+
+
+
 }
